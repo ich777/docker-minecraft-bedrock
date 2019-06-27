ef198c61a805fa02aaa40eb4eafe526e605cd6c8
ich777 <ich777@noreply.example.org>
2019-06-27 18:05:00 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,13 +4,13 @@ INS_V="$(find ${SERVER_DIR} -name *.installed | cut -d '-' -f 3 | awk -F ".insta
 if [ -z "$INS_V" ]; then
 	echo "---Minecraft Bedrock not found, Downloading v${GAME_VERSION}---"
 	cd ${SERVER_DIR}
-	wget -qi bedrock-server-${GAME_VERSION}.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-${GAME_VERSION}.zip
+	wget -qO bedrock-server-${GAME_VERSION}.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-${GAME_VERSION}.zip
     sleep 2
     unzip -o bedrock-server-${GAME_VERSION}.zip
     rm bedrock-server-${GAME_VERSION}.zip
     touch bedrock-server-${GAME_VERSION}.installed
     mv ${SERVER_DIR}/server.properties ${SERVER_DIR}/vanilla.server.properties
-    wget -qi server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-bedrock/master/config/server.properties
+    wget -qO server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-bedrock/master/config/server.properties
     if [ ! -f ${SERVER_DIR}/bedrock-server-${GAME_VERSION}.installed ]; then
     	echo "----------------------------------------------------------------------------------------------------"
     	echo "---Something went wrong, please install Minecraft Bedrock Server manually. Putting server into sleep mode---"
@@ -21,7 +21,7 @@ elif [ "${GAME_VERSION}" != "$INS_V" ]; then
 	echo "---Version missmatch Installed: v$INS_V - Prefered:${GAME_VERSION}, downloading v${GAME_VERSION}---"
 	rm ${SERVER_DIR}/bedrock-server-$INS_V.installed
 	cd ${SERVER_DIR}
-	wget -qi bedrock-server-${GAME_VERSION}.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-${GAME_VERSION}.zip
+	wget -qO bedrock-server-${GAME_VERSION}.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-${GAME_VERSION}.zip
 	sleep 2
     echo "---Creating Backup of config files---"
     mkdir ${SERVER_DIR}/backup_config_files
@@ -56,7 +56,7 @@ chmod -R 770 ${DATA_DIR}
 echo "---Checking for 'server.properties'---"
 if [ ! -f ${SERVER_DIR}/server.properties ]; then
     echo "---No 'server.properties' found, downloading...---"
-    wget -qi ${SERVER_DIR}/server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-bedrock/master/config/server.properties
+    wget -qO ${SERVER_DIR}/server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-bedrock/master/config/server.properties
 else
     echo "---'server.properties' found..."
 fi
