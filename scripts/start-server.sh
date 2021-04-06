ffc1abcb9914a222643045515d3eb4c29e873e07
ich777 <ich777@noreply.example.org>
2021-04-06 17:45:08 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -101,6 +101,8 @@ echo "---Starting Server---"
 cd ${SERVER_DIR}
 LD_LIBRARY_PATH=. && screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/bedrock_server ${GAME_PARAMS}
 sleep 2
-/opt/scripts/start-gotty.sh 2>/dev/null &
+if [ "${ENABLE_WEBCONSOLE}" == "true" ]; then
+    /opt/scripts/start-gotty.sh 2>/dev/null &
+fi
 screen -S watchdog -d -m /opt/scripts/start-watchdog.sh
 tail -f ${SERVER_DIR}/masterLog.0
